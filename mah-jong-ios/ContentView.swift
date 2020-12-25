7a3bd7392c2cbc619178b3a2f0861b2ef994a626
kylin-zhuo <lin.zhuo@hotmail.com>
2020-12-24 19:08:56 -0800

    first commit

---
+++ mah-jong-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mah-jong-ios
+//
+//  Created by Zhuo, Lin on 12/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mah-jong-ios/mah_jong_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mah_jong_iosApp.swift
+//  mah-jong-ios
+//
+//  Created by Zhuo, Lin on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct mah_jong_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
